8b9f0e9c3949ad56bb1ea4c8323cb7b7d9794850
Nick Reinoso <nick.reinoso@bitbuilders.com>
2025-07-09 13:04:32 -0500

    chore: update routing for quick links

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,10 +1,15 @@
 import { CommonModule } from '@angular/common';
-import { Component, HostBinding, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  HostBinding,
+  inject,
+  ViewEncapsulation,
+} from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { Title } from '@angular/platform-browser';
-import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 import { FeaturedItem } from '../../shared/models';
 
 const FEATURED_DUMMY: FeaturedItem[] = [
@@ -43,14 +48,14 @@ const FEATURED_DUMMY: FeaturedItem[] = [
 ];
 
 const QUICK_LINKS = [
-  { name: 'Bread', icon: 'breakfast_dining', path: '/recipes' },
-  { name: 'Breakfast', icon: 'bakery_dining', path: '/recipes' },
-  { name: 'Brownies', icon: 'oven_gen', path: '/recipes' },
-  { name: 'Cakes', icon: 'cake', path: '/recipes' },
-  { name: 'Cookies', icon: 'cookie', path: '/recipes' },
-  { name: 'Desserts', icon: 'icecream', path: '/recipes' },
-  { name: 'Pies', icon: 'blur_circular', path: '/recipes' },
-  { name: 'Tarts', icon: 'nutrition', path: '/recipes' },
+  { name: 'Bread', icon: 'breakfast_dining', path: 'bread' },
+  { name: 'Breakfast', icon: 'bakery_dining', path: 'breakfast' },
+  { name: 'Brownies', icon: 'oven_gen', path: 'brownies' },
+  { name: 'Cakes', icon: 'cake', path: 'cakes' },
+  { name: 'Cookies', icon: 'cookie', path: 'cookies' },
+  { name: 'Desserts', icon: 'icecream', path: 'desserts' },
+  { name: 'Pies', icon: 'blur_circular', path: 'pies' },
+  { name: 'Tarts', icon: 'nutrition', path: 'tarts' },
 ];
 
 @Component({
@@ -59,15 +64,11 @@ const QUICK_LINKS = [
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   encapsulation: ViewEncapsulation.None,
-  imports: [
-    MatIconModule,
-    MatButtonModule,
-    MatGridListModule,
-    CommonModule,
-    RouterLink,
-  ],
+  imports: [MatIconModule, MatButtonModule, MatGridListModule, CommonModule],
 })
 export class HomeComponent {
+  private router = inject(Router);
+
   @HostBinding('class') readonly className = 'home';
 
   public readonly featuredItems: FeaturedItem[] = FEATURED_DUMMY;
@@ -75,5 +76,9 @@ export class HomeComponent {
 
   constructor(title: Title) {
     title.setTitle('The Caffeinated Baker');
+  }
+
+  routeToRecipes(target: string): void {
+    this.router.navigate(['/recipes'], { queryParams: { category: target } });
   }
 }
