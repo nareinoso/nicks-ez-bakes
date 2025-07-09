e42ed702415c7280aec501b3aedf4f6a3bc651c7
Nick Reinoso <nick.reinoso@bitbuilders.com>
2025-07-09 12:32:39 -0500

    chore: add quick links

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -4,6 +4,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { Title } from '@angular/platform-browser';
+import { RouterLink } from '@angular/router';
 import { FeaturedItem } from '../../shared/models';
 
 const FEATURED_DUMMY: FeaturedItem[] = [
@@ -41,18 +42,36 @@ const FEATURED_DUMMY: FeaturedItem[] = [
   },
 ];
 
+const QUICK_LINKS = [
+  { name: 'Bread', icon: 'breakfast_dining', path: '/recipes' },
+  { name: 'Breakfast', icon: 'bakery_dining', path: '/recipes' },
+  { name: 'Brownies', icon: 'oven_gen', path: '/recipes' },
+  { name: 'Cakes', icon: 'cake', path: '/recipes' },
+  { name: 'Cookies', icon: 'cookie', path: '/recipes' },
+  { name: 'Desserts', icon: 'icecream', path: '/recipes' },
+  { name: 'Pies', icon: 'blur_circular', path: '/recipes' },
+  { name: 'Tarts', icon: 'nutrition', path: '/recipes' },
+];
+
 @Component({
   standalone: true,
   selector: 'ez-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   encapsulation: ViewEncapsulation.None,
-  imports: [MatIconModule, MatButtonModule, MatGridListModule, CommonModule],
+  imports: [
+    MatIconModule,
+    MatButtonModule,
+    MatGridListModule,
+    CommonModule,
+    RouterLink,
+  ],
 })
 export class HomeComponent {
   @HostBinding('class') readonly className = 'home';
 
   public readonly featuredItems: FeaturedItem[] = FEATURED_DUMMY;
+  public readonly quickLinks = QUICK_LINKS;
 
   constructor(title: Title) {
     title.setTitle('The Caffeinated Baker');
